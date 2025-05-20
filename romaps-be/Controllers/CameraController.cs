9db082513f69007e1f0546651b97c13d29ffe2c3
stefanm95 <stefan.moise@andnet.ro>
2025-05-20 14:45:06 +0300

    add project files

---
+++ romaps-be/Controllers/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using romaps_be.Models;
+using romaps_be.Settings;
+
+namespace romaps_be.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CameraController : ControllerBase
+    {
+        private readonly IMongoCollection<Camera> _cameras;
+
+        public CameraController(IMongoClient client, IOptions<MongoDbSettings> settings)
+        {
+            var database = client.GetDatabase(settings.Value.DatabaseName);
+            _cameras = database.GetCollection<Camera>("Cameras");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCameras()
+        {
+            var cameras = await _cameras.Find(_ => true).ToListAsync();
+            return Ok(cameras);
+        }
+    }
+}
---
+++ romaps-be/Program.cs
@@ -0,0 +1,48 @@
+using romaps_be.Settings;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy
+            .WithOrigins("http://localhost:5173", "https://localhost:5173") // Update this to your frontend's URL/port
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+// Add MongoDB settings
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+// Add MongoDB context
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
